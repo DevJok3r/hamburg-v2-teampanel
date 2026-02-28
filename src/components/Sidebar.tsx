a66c9bac134b495fadb8da08c7f8b90decea293a
DevJok3r <rainbow.jerome2@gmail.com>
2026-02-28 16:55:39 +0100

    feat: exams system with departments, questions, sessions

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -89,6 +89,12 @@ export default function Sidebar({ profile }: SidebarProps) {
       show: isTopManagement,
     },
     {
+      href: '/dashboard/exams',
+      label: 'Prüfungen',
+      icon: 'M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01',
+      show: isStaffRole,
+    },
+    {
       href: '/dashboard/dept-applications',
       label: 'Bewerbungen',
       icon: 'M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z',
